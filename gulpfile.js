ecb7fe29418cc9aa6e0d11fea25bb856ae365154
mdemo <mengdesen09@qq.com>
2014-05-15 17:33:14 +0800

    add some styl

--- gulpfile.js
+++ gulpfile.js
@@ -8,10 +8,15 @@ var stylus = require('gulp-stylus');
 gulp.task('selectors', function(){
    gulp.src('./selectors/selectors.styl')
        .pipe(stylus())
-       .pipe(gulp.dest('./selectors'));
-   gulp.watch('./selectors/selectors.styl',['selectors']);
+       .pipe(gulp.dest('./bulid'));
+});
+gulp.task('variables', function(){
+    gulp.src('./variables/variables.styl')
+        .pipe(stylus())
+        .pipe(gulp.dest('./bulid'));
 });
 gulp.task('watch', function(){
-
+    gulp.watch('./variables/variables.styl',['variables']);
+    gulp.watch('./selectors/selectors.styl',['selectors']);
 });
-gulp.task('default', ['selectors']);
+gulp.task('default', ['selectors','variables','watch']);
