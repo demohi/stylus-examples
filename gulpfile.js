d5e50b80bb4d8c77ecaed1daae94b5343e2118b1
mdemo <mengdesen09@qq.com>
2014-05-16 13:08:23 +0800

    update

--- gulpfile.js
+++ gulpfile.js
@@ -4,19 +4,25 @@
 
 var gulp = require('gulp');
 var stylus = require('gulp-stylus');
-
+var nib = require('nib');
 gulp.task('selectors', function(){
    gulp.src('./selectors/selectors.styl')
        .pipe(stylus())
-       .pipe(gulp.dest('./bulid'));
+       .pipe(gulp.dest('./build'));
 });
 gulp.task('variables', function(){
     gulp.src('./variables/variables.styl')
         .pipe(stylus())
-        .pipe(gulp.dest('./bulid'));
+        .pipe(gulp.dest('./build'));
+});
+gulp.task('nib', function(){
+    gulp.src('./nib/nib.styl')
+        .pipe(stylus({ use: nib(), compress: true, errors: true }))
+        .pipe(gulp.dest('./build'));
 });
 gulp.task('watch', function(){
     gulp.watch('./variables/variables.styl',['variables']);
     gulp.watch('./selectors/selectors.styl',['selectors']);
+    gulp.watch('./nib/nib.styl',['nib']);
 });
-gulp.task('default', ['selectors','variables','watch']);
+gulp.task('default', ['selectors','variables','nib','watch']);
